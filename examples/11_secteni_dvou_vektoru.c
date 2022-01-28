54cc97ab0d423938a47b80485f333bed1ed4ce67
Wanzaz <77801991+Wanzaz@users.noreply.github.com>
2022-01-28 07:31:40 +0100

    Update 11_secteni_dvou_vektoru.c

--- examples/11_secteni_dvou_vektoru.c
+++ examples/11_secteni_dvou_vektoru.c
@@ -19,18 +19,16 @@ void vektor(int a[], int b[], int vektory[], int n)
 	int i, soucet=0;
 	for(i=0;i<n;i++)
 	{
-		vektory [i] += a[i] + b[i];
+		vektory [i] = a[i] + b[i];
 	}	
 }
 
 
 int main()
 {
-    int n, x;
+    int n;
     printf("Zadej pocet prvku v poli: ");
     scanf("%d", &n);
-    printf("To cislo, ktere hledas: ");
-    scanf("%d", &x);
 
     int a[n], b[n], vektory[n];
 
